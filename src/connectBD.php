8552bfe7a33425d75aa2d3133bbb4354589bfc37
Luis Iriarte <eluigy@gmail.com>
2024-07-16 10:08:47 -0300

    + Base de datos

--- src/connectBD.php
+++ src/connectBD.php
@@ -1,9 +1,9 @@
 <?php
 const PORT ="3306";
-const HOST ="localhost";
-const DATABASE ="acceso_externos";
-const USER ="root";
-const PASSWORD ="";
+const HOST ="db";
+const DATABASE ="dni_scanner_gba";
+const USER ="my_user";
+const PASSWORD ="my_password";
 
 $dbconn = mysqli_connect(HOST,USER,PASSWORD,DATABASE, PORT)
 or die('Could not connect: ' . mysqli_error());
